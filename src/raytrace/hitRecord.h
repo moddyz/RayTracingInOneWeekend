bba732cedd158ec4be46c8b16270a952bcf28e49
moddyz <moddy3d@gmail.com>
2020-07-06 21:51:59 +1200

    Update HitRecord into a class.

--- src/raytrace/hitRecord.h
+++ src/raytrace/hitRecord.h
@@ -11,12 +11,13 @@
 
 RAYTRACE_NS_OPEN
 
-/// \struct HitRecord
+/// \class HitRecord
 ///
 /// HitRecord stores a record of a ray hitting a scene object, so that it may be used to influence
 /// the behavior of the next hit (of the same ray, with another object!).
-struct HitRecord
+class HitRecord
 {
+public:
     /// The position of the contact point.
     gm::Vec3f m_position;
 
