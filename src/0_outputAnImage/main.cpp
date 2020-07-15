236ea5d6ae92be3ed1169fb7249075824149b6e9
moddyz <moddy3d@gmail.com>
2020-07-15 18:09:27 +1200

    Cleaned up 0_outputAnImage

--- src/0_outputAnImage/main.cpp
+++ src/0_outputAnImage/main.cpp
@@ -1,19 +1,14 @@
-//
-// Example program for writing a PPM image onto disk.
-//
-
-#include <cxxopts.hpp>
-
-#define GM_DEBUG
-#include <gm/types/vec3f.h>
-
 #include <raytrace/imageBuffer.h>
 #include <raytrace/ppmImageWriter.h>
+
+#include <gm/types/vec3f.h>
+
+#include <cxxopts.hpp>
 
 int main( int i_argc, char** i_argv )
 {
     // Parse command line arguments.
-    cxxopts::Options options( "ppmExample", "Example program of writing out an PPM image." );
+    cxxopts::Options options( "ppmExample", "Example program of writing out an PPM image onto disk." );
     options.add_options()                                                                       // Command line options.
         ( "w,width", "Width of the image.", cxxopts::value< int >()->default_value( "384" ) )   // Width
         ( "h,height", "Height of the image.", cxxopts::value< int >()->default_value( "256" ) ) // Height;
@@ -24,16 +19,16 @@ int main( int i_argc, char** i_argv )
     int         height   = args[ "height" ].as< int >();
     std::string filePath = args[ "output" ].as< std::string >();
 
-    // Author a gradient image.
+    // Author a image, with a 2 dimensional gradient transition.
     raytrace::RGBImageBuffer image( width, height );
     for ( const gm::Vec2i& pixelCoord : image.Extent() )
     {
-        image( pixelCoord.X(), pixelCoord.Y() ) = gm::Vec3f( double( pixelCoord.X() ) / ( image.Width() - 1 ),
-                                                             double( pixelCoord.Y() ) / ( image.Height() - 1 ),
+        image( pixelCoord.X(), pixelCoord.Y() ) = gm::Vec3f( float( pixelCoord.X() ) / ( image.Width() - 1 ),
+                                                             float( pixelCoord.Y() ) / ( image.Height() - 1 ),
                                                              0.25f );
     }
 
-    // Write to disk.
+    // Write onto disk.
     if ( !raytrace::WritePPMImage( image, filePath ) )
     {
         return -1;
